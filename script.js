6037194ea8fca1289b9b326864b088c0addc8d5c
Sang Bae <sw2.bae@gmail.com>
2020-04-08 02:34:43 -0500

    hw05_rev02_tableDraft

--- script.js
+++ script.js
@@ -1,68 +1,44 @@
-$(document).ready(function(event) {
+$(document).ready(function (event) {
 
-var currentTime = moment().format('dddd MMMM Do YYYY, h:mm a');
-var timeIndex = moment().format('HH');
+    var currentTime = moment().format('dddd MMMM Do YYYY, h:mm a');
+    var timeIndex = moment().format('HH');
+
+    function timeWriting() {
+        $("#currentDay").text(currentTime);
+    }
 
 
-
-function timeWriting(){
-    $("#currentDay").text(currentTime);
-}
-
-function tableMaker() {
-
-    var trMaker = $("<tr>");
-    var tdMaker = $("<td>");
-    var saveBtn = $("<button>");
+    timeWriting();
 
 
 
-    for (var i=9; i<18;i++){
+
+    for (var i = 9; i < 18; i++) {
+
+        var trMaker = $("<tr>");
+
+        var tdHour = $("<td>");
+        var inputMaker = $("<input>");
+        var saveBtn = $("<button>");
+
+        trMaker.addClass("row");
+
+        tdHour.addClass("hour col-2");
+        tdHour.text(i);
+        tdHour.appendTo(trMaker);
+
+        inputMaker.addClass("description col-9");
+        inputMaker.attr("placeholder",i);
+        inputMaker.appendTo(trMaker);
+
+        saveBtn.addClass("saveBtn col-1");
+        saveBtn.text(i);
+        saveBtn.appendTo(trMaker);
+
+        trMaker.appendTo($(".container"));
 
 
+    }
 
-        
-
-    //row
-        // trMaker
-        //     .addClass("row")
-        //     
-
-       
-    
-    // hours
-        // tdMaker
-        //     .addClass("time-block hour")
-        //     .text()
-    // description
-        // tdMaker
-        //     .addClass("description")
-
-    // saveBtn
-        // saveBtn
-        //     .addClass("saveBtn")
-}
-
-
-
-timeWriting();
 
 });
-
-
-
-    // <header class="jumbotron">
-    //   <h1 class="display-3">Work Day Scheduler</h1>
-    //   <p class="lead">A simple calendar app for scheduling your work day</p>
-    //   <p id="currentDay" class="lead"></p>
-    // </header>
-    // <div class="container">
-    //   <!-- Timeblocks go here -->
-    // </div> */}
-
-
-//     <tr>
-//     <td>Interstellar</td>
-//     <td>2014</td>
-//     <td>Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow</td>
-//   </tr>
